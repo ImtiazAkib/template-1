e851bf30cc0fda72c87a9197d04157a52f4e4384
ImtiazAkib <imtiazakib007@gmail.com>
2022-02-10 18:55:33 +0600

    new

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
 import "./Home.css";
 import mobile from "./mobile.png";
+import shape from "./shape-bg.png";
 import AppleIcon from "@mui/icons-material/Apple";
 import AndroidIcon from "@mui/icons-material/Android";
 import DesktopWindowsIcon from "@mui/icons-material/DesktopWindows";
 import PlayCircleOutlineIcon from "@mui/icons-material/PlayCircleOutline";
+import CloseIcon from "@mui/icons-material/Close";
+import EmojiEventsIcon from "@mui/icons-material/EmojiEvents";
 
 const Home = () => {
   const [showPopup, setShowPopup] = useState(false);
@@ -34,7 +37,10 @@ const Home = () => {
                 <DesktopWindowsIcon></DesktopWindowsIcon>
               </button>
               {/*------------ animatimated Button --------------------*/}
-              <button className="video-play-btn">
+              <button
+                className="video-play-btn"
+                onClick={() => setShowPopup(!showPopup)}
+              >
                 <PlayCircleOutlineIcon fontSize="large"></PlayCircleOutlineIcon>
               </button>
             </div>
@@ -43,29 +49,74 @@ const Home = () => {
             <img src={mobile} alt="" />
           </div>
         </div>
+        {/*------------ image shape -----------------*/}
+        <div className="shape">
+          <div className="shape-img">
+            <img src={shape} alt="" />
+          </div>
+        </div>
         <div className="banner-overlay"></div>
       </div>
+      {/*-------------------
+       banner section end
+       ---------------------- */}
       {/*----------------
        video popup start
        ------------------ */}
-      <div className="video-popup">
-        <div className="video-popup-inner">
-          <iframe
-            width="560"
-            height="315"
-            src="https://www.youtube.com/embed/uLvhAJfx3T0"
-            title="YouTube video player"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowfullscreen
-          ></iframe>
+      <div className="fixed-top">
+        <div className="video-popup" id={showPopup ? "hidden-popup" : ""}>
+          <div className="video-popup-inner">
+            <div className="iframe-popup">
+              <iframe
+                src="https://www.youtube.com/embed/uLvhAJfx3T0"
+                title="YouTube video player"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              ></iframe>
+            </div>
+          </div>
+          <div className="video-close-btn">
+            <button onClick={() => setShowPopup(!showPopup)}>
+              <CloseIcon></CloseIcon>
+            </button>
+          </div>
         </div>
       </div>
       {/*----------------
        video popup end
        ------------------ */}
-      {/*-------------------
-       banner section end
-       ---------------------- */}
+      {/*---------------------- user and download section -----------------------*/}
+      <div>
+        <div className="d-lg-flex justify-content-between container mt-5">
+          <div className="d-flex align-items-center justify-content-center mb-3">
+            <div>
+              <EmojiEventsIcon
+                style={{ color: "#281483" }}
+                fontSize="large"
+              ></EmojiEventsIcon>
+            </div>
+            <div className="ms-2 text-center">
+              <h3 style={{ color: "#747474" }}>Trusted by 4000+</h3>
+              <p style={{ color: "#787878" }}>High performing team worldwide</p>
+            </div>
+          </div>
+          <div className="d-flex align-items-center justify-content-center">
+            <div className="text-center">
+              <h3 style={{ color: "#281483" }}>145052</h3>
+              <p>Downloads</p>
+            </div>
+            <div className="text-center ms-5">
+              <h3 style={{ color: "#8f6ed5" }}>4400+</h3>
+              <p>Active Installs</p>
+            </div>
+            <div className="text-center ms-5">
+              <h3 style={{ color: "#d782d9" }}>5000+</h3>
+              <p>Paid Users</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/*---------------------- user and download section -----------------------*/}
     </div>
   );
 };
